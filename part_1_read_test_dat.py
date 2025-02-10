a9f3945862d928c2ed5801691a98c2481c2b3c08
neetakhanuja <neetakhanuja@gmail.com>
2025-02-10 12:53:55 -0500

    Added output from pfgd_test.dat and Tile World screenshot

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,17 @@
-import cc_dat_utils
+import cc_dat_utils  # Import the cc_dat_utils module
 
-#Part 1
-input_dat_file = "data/pfgd_test.dat"
+# Define the input .dat file
+dat_file = "data/pfgd_test.dat"
 
+# Use the function to load the level pack
+level_pack = cc_dat_utils.make_cc_level_pack_from_dat(dat_file)
 
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-#print the resulting data
+# Print the string representation of the level pack
+print(level_pack)
+
+# Save the output to a text file
+output_file = "data/pfgd_test.txt"
+with open(output_file, "w") as f:
+    f.write(str(level_pack))
+
+print(f"Output saved to {output_file}")
